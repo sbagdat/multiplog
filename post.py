896f5fa32f73eef760aa0b81c8e293915261e946
Sıtkı Bağdat <sbagdat@gmail.com>
2016-05-20 06:13:22 +0300

    feat: Show edit and delete button for posts
    If user is owner of the post, give them a button to edit and delete.

--- post.py
+++ post.py
@@ -9,9 +9,15 @@ class Post(db.Model):
     created_at = db.DateTimeProperty(auto_now_add=True)
     user_id = db.IntegerProperty(required=True)
 
-    def render(self):
+    def render(self, user=None):
         self._render_text = self.content.replace('\n', '<br>')
-        return render_str("_post.html", post=self)
+        return render_str("_post.html", post=self, user=user)
 
     def username(self):
         return User.find_by_id(self.user_id).user
+
+    def linkified_subject(self):
+        return self.subject.replace(' ', '-')
+
+    def unlinkified_subject(self):
+        return self.subject.replace('-', ' ')
